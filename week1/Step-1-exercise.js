6d39e1e3f564f9c7d8a76a95b37f48948905eee7
zoeyzou <zouying1991@aliyun.com>
2018-06-09 13:51:35 +0200

    feat: add js-week1-step1-question-3 answer

--- week1/Step-1-exercise.js
+++ week1/Step-1-exercise.js
@@ -5,3 +5,5 @@ const x = 20; // for non-reassignable variable
 // 2. How do you round the number 7.25, to the nearest integer?
 Math.round(7.25);
 
+// 3. Create a array called colors with the strings red, green and blue inside.
+const colors = ["red", "green", "blue"];

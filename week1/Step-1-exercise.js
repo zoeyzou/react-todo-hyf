bef60d8d8ce17d87d2ecc52822fa7c330f93e37a
zoeyzou <zouying1991@aliyun.com>
2018-06-09 13:52:00 +0200

    feat: add js-week1-step1-question-4 answer

--- week1/Step-1-exercise.js
+++ week1/Step-1-exercise.js
@@ -7,3 +7,7 @@ Math.round(7.25);
 
 // 3. Create a array called colors with the strings red, green and blue inside.
 const colors = ["red", "green", "blue"];
+
+// 4. How can you find the length of the string you just created?
+let strLength = colors.length;
+console.log(strLength);

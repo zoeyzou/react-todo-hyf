1e180ecc2ce261d247da28303d3eeb01ae3269e3
zoeyzou <zouying1991@aliyun.com>
2018-06-09 13:57:46 +0200

    feat: add js-week1-step1-question-6 answer

--- week1/Step-1-exercise.js
+++ week1/Step-1-exercise.js
@@ -23,3 +23,8 @@ function checkType(x, y) {
   }
 }
 checkType(x, y);
+
+// 6. If x equals 7, and the only other statement is x = x % 3, what would be the new value of x?
+let x = 7;
+x = x % 3;
+console.log(x); // 1

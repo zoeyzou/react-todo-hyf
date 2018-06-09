b00cc9fadf5a7998efd9c88169cd927f19cdffb8
zoeyzou <zouying1991@aliyun.com>
2018-06-09 13:55:04 +0200

    feat: add js-week1-step1-question-5 answer

--- week1/Step-1-exercise.js
+++ week1/Step-1-exercise.js
@@ -11,3 +11,15 @@ const colors = ["red", "green", "blue"];
 // 4. How can you find the length of the string you just created?
 let strLength = colors.length;
 console.log(strLength);
+
+// 5. Write a program that checks the types of two variables and prints out SAME TYPE if they are the same type. 
+let x = 10, y =  "red";
+function checkType(x, y) {
+  if (typeof x == typeof y) {
+    console.log("SAME TYPE");
+  }
+  else {
+    console.log("Oops, they are different types");
+  }
+}
+checkType(x, y);

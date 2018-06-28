7ef1972dedb4c89dd4e1ed93963a0ec7ded63049
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-06-28 11:12:21 +0200

    feat: add the rest homework and json challenge

--- JavaScript-2/week4/Week4Homework.js
+++ JavaScript-2/week4/Week4Homework.js
@@ -1,19 +1,22 @@
-/**
- * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
- * @param {integer} min - The min number
- * @param {integer} max - The max number
- * @returns {Number} Random number between min and max
- */
+// /**
+//  * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
+//  * @param {integer} min - The min number
+//  * @param {integer} max - The max number
+//  * @returns {Number} Random number between min and max
+//  */
+
 function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  let random = Math.floor(Math.random() * (max - min + 1) + min);
+  return random;
 }
 
 
-/**
-* Get an array with car objects with random color and speed
-* @param {integer} numberOfCars - The number of cars 
-* @returns {array} Array containing the car objects
-*/
+// /**
+// * Get an array with car objects with random color and speed
+// * @param {integer} numberOfCars - The number of cars 
+// * @returns {array} Array containing the car objects
+// */
+
 function generateCars(numberOfCars) {
   const cars = [];
 
@@ -36,18 +39,26 @@ function generateCars(numberOfCars) {
 }
 
 let generatedCars = generateCars(10);
+console.log('-> generated cars: ');
+console.log(generatedCars);
 
 //1.1: Cars with speeds between 30 and 60
 let slowCars = generatedCars.filter(car => car.speed > 30 && car.speed < 60);
+console.log('-> slow cars: ');
+console.log(slowCars);
 
 //1.2: The makes of the cars that are not lightyellow
 let nonLightyellowCars = 
     generatedCars.filter(car => car.color !== 'lightyellow').map(car => car.make);
+console.log('-> non light yellow cars: ');
+console.log(nonLightyellowCars);
 
 let nonRepeatedMake = 
     nonLightyellowCars.filter((elem, index, self) => index == self.indexOf(elem));
     //found the solution online, but not yet understood
     //solution: https://codehandbook.org/how-to-remove-duplicates-from-javascript-array/
+console.log('-> non-repeated make: ');
+console.log(nonRepeatedMake);
 
 //1.3 Change to Danish
 
@@ -60,5 +71,7 @@ function modifyKey(oldCars) {
 }
 
 let mapNewCars = generatedCars.map(modifyKey);
+console.log('-> map new cars: ');
+console.log(mapNewCars);
 
 

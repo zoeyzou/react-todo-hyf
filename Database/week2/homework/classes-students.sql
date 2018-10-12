6e06dbd34fabbec908d4b9d738798a140ce35c62
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-10-12 22:24:08 +0200

    add: add the first row to sql query

--- Database/week2/homework/classes-students.sql
+++ Database/week2/homework/classes-students.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `class` (
+SET NAMES utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `class` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `begins` DATETIME NOT NULL,
@@ -6,14 +8,17 @@ CREATE TABLE `class` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `students` (
+
+CREATE TABLE IF NOT EXISTS `students` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
     `phone` varchar(255) NULL,
+    `class_id` int(10) unsigned NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- classes
 INSERT INTO class (id, name, begins, ends) VALUES (1, 'History', '2018-10-29 08:00:00', '2019-01-20 17:00:00');
@@ -23,12 +28,12 @@ INSERT INTO class (id, name, begins, ends) VALUES (3, 'Geography', '2018-11-12 1
 
 -- students
 INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Aarika Ellingworth', 'aellingworth0@harvard.edu', '483-396-8795', 1);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Pren Goldsworthy', 'pgoldsworthy1@spotify.com', '635-572-8467', 2);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Pablo Kisbee', 'pkisbee2@lulu.com', '790-962-8683', 1);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Rodie Duncan', 'rduncan3@quantcast.com', '646-743-6191', 3);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Aubry Polak', 'apolak4@indiatimes.com', '302-678-7931', 2);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Maryrose Meadows', 'mmeadows5@comcast.net', '251-524-6594', 1);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Pavel Brushneen', 'pbrushneen6@techcrunch.com', '316-170-3640', 3);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'Hedy Gerault', 'hgerault7@nymag.com', '176-177-5579', 1);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, '王秀英', 'wang.xiuying@weebly.com', '891-952-6749', 2);
-INSERT INTO students (id, name, email, phone, class_id) VALUES (1, 'إلياس', 'elias@github.com', '202-517-6983', 3);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (2, 'Pren Goldsworthy', 'pgoldsworthy1@spotify.com', '635-572-8467', 2);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (3, 'Pablo Kisbee', 'pkisbee2@lulu.com', '790-962-8683', 1);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (4, 'Rodie Duncan', 'rduncan3@quantcast.com', '646-743-6191', 3);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (5, 'Aubry Polak', 'apolak4@indiatimes.com', '302-678-7931', 2);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (6, 'Maryrose Meadows', 'mmeadows5@comcast.net', '251-524-6594', 1);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (7, 'Pavel Brushneen', 'pbrushneen6@techcrunch.com', '316-170-3640', 3);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (8, 'Hedy Gerault', 'hgerault7@nymag.com', '176-177-5579', 1);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (9, '王秀英', 'wang.xiuying@weebly.com', '891-952-6749', 2);
+INSERT INTO students (id, name, email, phone, class_id) VALUES (10, 'إلياس', 'elias@github.com', '202-517-6983', 3);
